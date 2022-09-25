535ea6a9f1d1c04010ae5c0731b2cbb117881eb8
KatieJessupMcD <katie.jessupmcd@gmail.com>
2022-09-24 20:28:45 -0700

    remove extra type from EnsembleType

--- app/graphql/types/ensemble_type.rb
+++ app/graphql/types/ensemble_type.rb
@@ -6,7 +6,6 @@ module Types
     field :name, String, null: false
     field :school_id, Integer, null: false
     field :school, Types::SchoolType, null: false
-    field :school, Types::SchoolType, null: false
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
   end
